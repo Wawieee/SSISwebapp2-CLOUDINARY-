78cfa9cddd7613f408c4ddbefd004db27814615e
Nawawi Palawan <126594145+Wawieee@users.noreply.github.com>
2023-12-11 22:14:50 +0800

    Add files via upload

---
+++ ssiswebappDB.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE ssiswebapp;
+USE ssiswebapp;
+
+CREATE TABLE college (
+    code VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE course (
+    code VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    college_code VARCHAR(10),
+    FOREIGN KEY (college_code) REFERENCES college(code)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE student (
+    id CHAR(9) PRIMARY KEY,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    course_code VARCHAR(10),
+    year INT,
+    gender ENUM('Male', 'Female', 'Other') NOT NULL,
+    FOREIGN KEY (course_code) REFERENCES course(code)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
